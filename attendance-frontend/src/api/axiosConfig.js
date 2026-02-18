e6691ce040724842ce8d9d74e3878e1cf6fa240b
siva100625 <sivasubramanian.v2023ece@sece.ac.ingamil.com>
2026-02-18 18:41:07 +0530

    frontend commit

--- attendance-frontend/src/api/axiosConfig.js
+++ attendance-frontend/src/api/axiosConfig.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8080"
+  baseURL: "https://smart-attendance-backend-0ggj.onrender.com"
 });
-
+//http://localhost:8080
 axiosInstance.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) config.headers.Authorization = `Bearer ${token}`;
